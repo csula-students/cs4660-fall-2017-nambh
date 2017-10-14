b82aa6950fa1517a9748ec61f25a6f1192e5f7da
nambh <namsc27@gmail.com>
2017-10-14 10:00:17 -0700

    First Done q2

--- cs4660/quiz/main.py
+++ cs4660/quiz/main.py
@@ -21,6 +21,64 @@ except ImportError:
 GET_STATE_URL = "http://192.241.218.106:9000/getState"
 STATE_TRANSITION_URL = "http://192.241.218.106:9000/state"
 
+def bfs(graph, initial_node, dest_node):
+    parent = {}
+    parent[initial_node] = None
+    visited_nodes = []
+    visited_nodes.append(initial_node)
+    distance = {}
+    distance[initial_node] = 0
+    Q = []
+    Q.append(initial_node)
+    last_node = dest_node
+    while (bool(Q)):
+        current_node = Q.pop(0)
+        for neighbor in graph.neighbors(current_node):
+            if (neighbor not in visited_nodes):
+                    Q.append(neighbor)
+                    distance[neighbor] = distance[current_node] + graph.distance(current_node,neighbor)
+                    parent[neighbor] = current_node
+                    visited_nodes.append(neighbor)
+        if (dest_node in visited_nodes):
+            break
+    path = []
+    while parent[last_node] is not None:
+        path = [graph.get_edge(parent[last_node],last_node)]+ path
+        last_node = parent[last_node]
+    return path
+
+def dijkstra_search(graph, initial_node, dest_node):
+    parent = {}
+    parent[initial_node] = None
+    visited_nodes = []
+    visited_nodes.append(initial_node)
+    distance = {}
+    distance[initial_node] = 0
+    temp_nodes = []
+    Q = {}
+    Q[initial_node] = 0
+    last_node = dest_node
+    while (bool(Q)):
+        current_node = min(Q, key=Q.get)
+        Q.pop(current_node)
+        visited_nodes.append(current_node)
+        if (dest_node in visited_nodes):
+            break
+        for neighbor in graph.neighbors(current_node):
+            if ((neighbor not in visited_nodes and neighbor not in temp_nodes) or (distance[neighbor]>distance[current_node] + graph.distance(current_node, neighbor))):
+                Q[neighbor] = distance[current_node] + graph.distance(current_node, neighbor)
+                distance[neighbor] = distance[current_node] + graph.distance(current_node, neighbor)
+                parent[neighbor] = current_node
+                temp_nodes.append(neighbor)
+
+    path = []
+    while parent[last_node] is not None:
+        path = [graph.get_edge(parent[last_node], last_node)] + path
+        last_node = parent[last_node]
+
+    return path
+
+
 def get_state(room_id):
     """
     get the room by its id and its neighbor
@@ -53,5 +111,18 @@ def __json_request(target_url, body):
 if __name__ == "__main__":
     # Your code starts here
     empty_room = get_state('7f3dc077574c013d98b2de8f735058b4')
+    dark_room = get_state('f1f131f647621a4be7c71292e79613f9')
+
+    print("BFS Path:")
     print(empty_room)
     print(transition_state(empty_room['id'], empty_room['neighbors'][0]['id']))
+    BFS_Path = bfs(transition_state(empty_room['id'], empty_room['neighbors'][0]['id']), empty_room, dark_room)
+    print("Total hp: ")
+    print(len(BFS_Path))
+
+    print("Dijkstra Path:")
+    print(empty_room)
+    print(transition_state(empty_room['id'], empty_room['neighbors'][0]['id']))
+    Dijkstra_Path = dijkstra_search(transition_state(empty_room['id'], empty_room['neighbors'][0]['id']), empty_room, dark_room)
+    print("Total hp:")
+    print(len(Dijkstra_Path))
